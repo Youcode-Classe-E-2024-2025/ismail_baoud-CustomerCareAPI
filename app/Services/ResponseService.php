ab087053ac8ce7140590233292814a2855efd09e
ismailbaoud <ibaoud46@gmail.com>
2025-03-21 12:39:12 +0000

    ajouter le service des reponses

---
+++ app/Services/ResponseService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+class ResponseServices{
+    
+    public function AuthUser(){
+        
+    }
+
+    public function handel(){
+
+    }
+
+    public function handelEdit(){
+        
+    }
+
+    public function handeldestroy(){
+        
+    }
+}
