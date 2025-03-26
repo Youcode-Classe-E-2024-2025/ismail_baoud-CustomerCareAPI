bd3dce2481c4e26e2dbe2fc19a84da0543cc31c7
ismailbaoud <ibaoud46@gmail.com>
2025-03-26 10:50:57 +0000

    add the megration of response

--- CustomerCareAPI/database/migrations/2025_03_21_104030_create_responses_table.php
+++ CustomerCareAPI/database/migrations/2025_03_21_104030_create_responses_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('responses', function (Blueprint $table) {
             $table->id();
-            $table->text('description');
-            $table->text('content_vusial');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->text('message');
+            $table->foreignId('ticket_id')->constrained('tickets')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // العميل أو الوكيل
+            $table->timestamps();
         });
     }
 
