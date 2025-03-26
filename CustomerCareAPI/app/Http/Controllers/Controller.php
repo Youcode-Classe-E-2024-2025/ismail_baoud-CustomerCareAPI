0c5c1c78211ead7d5f33e0d2a3d36bfaf8cff1b6
ismailbaoud <ibaoud46@gmail.com>
2025-03-26 10:39:00 +0000

    Added traits for authorization, job dispatching, and validation to Controller

--- CustomerCareAPI/app/Http/Controllers/Controller.php
+++ CustomerCareAPI/app/Http/Controllers/Controller.php
@@ -2,7 +2,19 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *     title="Customer Care API",
+ *     version="1.0.0",
+ *     description="API documentation for managing support tickets"
+ * )
+ */
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
